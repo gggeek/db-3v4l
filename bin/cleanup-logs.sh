ccaf6805061f7bbfd6da48b13f37dc9a7f86b4a1
Gaetano Giunta <ggiunta@kaliop.com>
2019-04-07 13:55:10 +0000

    allow to clean up docker logs

--- bin/cleanup-logs.sh
+++ bin/cleanup-logs.sh
@@ -2,19 +2,25 @@
 
 # removes log files
 
+CLEAN_DOCKER=false
+
 function help() {
     echo -e "Usage: cleanup-logs.sh [OPTION]
 
 Removes Databases, Nginx, PHP logs. Better run from the host computer while Docker is off
 
 Options:
+    -d              clear Docker logs as well (needs root perms)
     -h              print help
 "
 }
 
-while getopts ":h" opt
+while getopts ":dh" opt
 do
     case $opt in
+        d)
+            CLEAN_DOCKER=true
+        ;;
         h)
             help
             exit 0
@@ -34,5 +40,10 @@ find ./docker/logs/ -type f ! -name .gitkeep -delete
 
 find ./app/var/log/ -type f ! -name .gitkeep -delete
 
-# Note: we could attempt to clear the docker logs, but we'd need to be root. Example command:
-# echo "" > $(docker inspect --format='{{.LogPath}}' <container_name_or_id>
+cd docker
+if [ ${CLEAN_DOCKER} = 'true' ]; then
+    for CONTAINER in $(docker-compose ps -q)
+    do
+        echo "" > $(docker inspect --format='{{.LogPath}}' ${CONTAINER})
+    done
+fi
